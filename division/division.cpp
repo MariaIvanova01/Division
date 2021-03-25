de5c0f6a5cd8b0dbb6a4288f48469d1dd33172ef
MariaIvanova01 <mimi200801@abv.bg>
2021-03-25 18:10:16 +0200

    Dividing two numbers with and without remainder

---
+++ division/division.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float n, m, k, d;
+    cout << " n = ";
+    cin >> n;
+    cout << "m = ";
+    cin >> m;
+    k = n / m;
+    cout << k << endl;
+    d = n % m;
+    cout << d << endl;
+}
+
